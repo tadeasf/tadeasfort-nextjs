74d86fdf9c2c49f13dc802f067dca485285fde61
tadeasf <taddy.fort@gmail.com>
2023-07-25 15:41:51 +0200

    huehue

--- app/page.tsx
+++ app/page.tsx
@@ -29,13 +29,13 @@ export default function Home() {
 				quantity={100}
 			/>
 			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-				Fořt
+				Tadeáš Fořt
 			</h1>
 
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 ">
-					Hi, my name is Tadeáš.
+					Hi, I'm a data engineer by day and web developer by night.
 				</h2>
 			</div>
 		</div>
