c780b7fd8b6d0494f0a1557a8e0d36bbe67b042b
tadeasf <taddy.fort@gmail.com>
2023-07-25 22:27:18 +0200

    useeffect

--- app/components/analytics.tsx
+++ app/components/analytics.tsx
@@ -1,9 +1,21 @@
 "use client";
 
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 export function Analytics() {
   const googleAnalyticsId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
+
+  useEffect(() => {
+    // Get the current page slug
+    const slug = window.location.pathname;
+
+    // Send a POST request to your `/api/incr` endpoint
+    axios.post('/api/incr', { slug })
+      .catch(error => {
+        console.error('Error incrementing page view count:', error);
+      });
+  }, []);
 
   if (!googleAnalyticsId) {
     return null;
