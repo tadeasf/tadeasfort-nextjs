be7cd6a0c93119cea403aae0b590173cf3c4ba55
tadeasf <taddy.fort@gmail.com>
2023-07-26 09:29:38 +0200

    idk

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -64,34 +64,33 @@ export default function Auth() {
   
 
   return (
-    <ProjectsLayout>
-      <div className="relative pb-16">
-        <Navigation />
-        <div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
-          <div className="max-w-2xl mx-auto lg:mx-0">
-            <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
-              Authentication
-            </h2>
-            <p className="mt-4 text-zinc-400">
-              Please login or sign up to continue.
-            </p>
-          </div>
-          <div className="w-full h-px bg-zinc-800" />
-          <div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2 ">
-            <div className="w-full max-w-md mx-auto lg:mx-0">
-              <AuthForm
-                form={form}
-                isLogin={isLogin}
-                message={message}
-                loading={loading}
-                handleFormChange={handleFormChange}
-                handleFormSubmit={handleFormSubmit}
-                toggleIsLogin={() => setIsLogin(!isLogin)}
-              />
-            </div>
+    <div className="relative pb-16">
+      <Navigation />
+      <div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
+        <div className="max-w-2xl mx-auto lg:mx-0">
+          <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
+            Authentication
+          </h2>
+          <p className="mt-4 text-zinc-400">
+            Please login or sign up to continue.
+          </p>
+        </div>
+        <div className="w-full h-px bg-zinc-800" />
+        <div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2 ">
+          <div className="w-full max-w-md mx-auto lg:mx-0">
+            <AuthForm
+              form={form}
+              isLogin={isLogin}
+              message={message}
+              loading={loading}
+              handleFormChange={handleFormChange}
+              handleFormSubmit={handleFormSubmit}
+              toggleIsLogin={() => setIsLogin(!isLogin)}
+            />
           </div>
         </div>
       </div>
-    </ProjectsLayout>
+    </div>
   );
 };
+
