cad4f21cde50e4bd44bcb32a0f754b2d793a6ea1
tadeasf <taddy.fort@gmail.com>
2023-07-26 08:37:23 +0200

    styling

--- app/components/authForm.tsx
+++ app/components/authForm.tsx
@@ -22,31 +22,69 @@ const AuthForm: React.FC<AuthFormProps> = ({
   toggleIsLogin,
 }) => {
   return (
-    <div>
-      <h1>{isLogin ? 'Log in' : 'Sign up'}</h1>
-      <form onSubmit={handleFormSubmit}>
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={form.email}
-          onChange={handleFormChange}
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={form.password}
-          onChange={handleFormChange}
-        />
-        <button type="submit" disabled={loading}>
-          {isLogin ? 'Log in' : 'Sign up'}
-        </button>
-        <button type="button" onClick={toggleIsLogin}>
-          Switch to {isLogin ? 'Sign up' : 'Log in'}
-        </button>
-      </form>
-      {message && <p>{message}</p>}
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-2 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            {isLogin ? 'Log in' : 'Sign up'}
+          </h2>
+        </div>
+        <form className="mt-8 space-y-6" onSubmit={handleFormSubmit}>
+          <input type="hidden" name="remember" value="true" />
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label htmlFor="email-address" className="sr-only">
+                Email address
+              </label>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Email address"
+                value={form.email}
+                onChange={handleFormChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Password"
+                value={form.password}
+                onChange={handleFormChange}
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              disabled={loading}
+            >
+              {isLogin ? 'Log in' : 'Sign up'}
+            </button>
+            <button
+              type="button"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-indigo-600 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mt-4"
+              onClick={toggleIsLogin}
+            >
+              Switch to {isLogin ? 'Sign up' : 'Log in'}
+            </button>
+          </div>
+        </form>
+        {message && <p className="mt-2 text-center text-sm text-red-600">{message}</p>}
+      </div>
     </div>
   );
 };
